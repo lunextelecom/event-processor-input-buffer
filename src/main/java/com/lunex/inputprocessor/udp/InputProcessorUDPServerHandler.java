fed003a1dfeafd2adcb1a0746431fe8eae94772e
baole-lunex <baole@lunextelecom.com>
2014-08-14 17:11:06 +0700

    process udp server with jsonobject

--- src/main/java/com/lunex/inputprocessor/udp/InputProcessorUDPServerHandler.java
+++ src/main/java/com/lunex/inputprocessor/udp/InputProcessorUDPServerHandler.java
@@ -1,5 +1,7 @@
 package com.lunex.inputprocessor.udp;
 
+import org.json.JSONObject;
+
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -12,11 +14,10 @@ public class InputProcessorUDPServerHandler extends
 	@Override
 	public void channelRead0(ChannelHandlerContext ctx, DatagramPacket packet) throws Exception {
 		//System.out.println(packet);
-		
-		String packageContent = packet.content().toString(CharsetUtil.UTF_8);
 		String responseContent = "Get package and Process async";
-		
-		if (packageContent.startsWith("async")) {		
+		String packageContent = packet.content().toString(CharsetUtil.UTF_8);
+		JSONObject jsonObject = new JSONObject(packageContent);
+		if (jsonObject.getBoolean("async")) {		
 			Thread thread = new Thread(new PackageProcessorThread(packet));
 			thread.start();
 			
@@ -26,7 +27,6 @@ public class InputProcessorUDPServerHandler extends
 		} else {
 			// TODO
 			responseContent = "result";
-			
 		}
 		// Sent response to client sender
 		ctx.write(new DatagramPacket(Unpooled.copiedBuffer(responseContent, CharsetUtil.UTF_8), packet.sender()));
