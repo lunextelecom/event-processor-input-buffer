2d53e84858c0b69423b5c65bc45a57a0e3548133
baole-lunex <baole@lunextelecom.com>
2014-08-20 16:07:13 +0700

    change object

--- src/main/java/com/lunex/inputprocessor/CallbackHTTPVisitor.java
+++ src/main/java/com/lunex/inputprocessor/CallbackHTTPVisitor.java
@@ -1,11 +1,11 @@
 package com.lunex.inputprocessor;
 
-import org.json.JSONObject;
-
 import io.netty.channel.ChannelHandlerContext;
 
 public class CallbackHTTPVisitor {
-	public JSONObject result;
+	
+	public Object result;
+
 	public void doJob(ChannelHandlerContext ctx, Object msg) {
 		return;
 	}
